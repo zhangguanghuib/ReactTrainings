3178d9618ba050ce9565dd16dd02313aef999d8e
Guanghui Zhang <guazha@microsoft.com>
2025-04-17 21:14:19 +0800

    Guanghui base 01

---
+++ qiangfeng_guanghui/src/01-base/01-class组件.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class KerwinApp extends React.Component{
+  render(){
+      return (
+          <section>
+              hello react Component
+              <ul>
+                  <li>1111</li>
+                  <li>2222</li>
+              </ul>
+              <div>新的内容111</div>  
+              <div>新的内容2222</div>  
+          </section>
+      )
+  }
+}
+
+
+export default KerwinApp
